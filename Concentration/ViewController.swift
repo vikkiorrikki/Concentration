762c47decfb028bee43f9c5a1168da61718e29e2
Victoria Saklakova <viksi.saklakova@gmail.com>
2020-03-09 20:46:24 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Виктория Саклакова on 09.03.2020.
+//  Copyright © 2020 Viktoriia Saklakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
